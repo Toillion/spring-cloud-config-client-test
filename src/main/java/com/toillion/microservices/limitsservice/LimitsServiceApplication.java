16ad936b6dd254447b072afd11256193e5e24df6
Paul Toillion <paul.toillion@abcfinancial.com>
2020-05-20 08:33:40 -0500

    created client

---
+++ src/main/java/com/toillion/microservices/limitsservice/LimitsServiceApplication.java
@@ -0,0 +1,15 @@
+package com.toillion.microservices.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServiceApplication
+{
+
+   public static void main( String[] args )
+   {
+      SpringApplication.run( LimitsServiceApplication.class, args );
+   }
+
+}
---
+++ src/test/java/com/toillion/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.toillion.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests
+{
+
+   @Test
+   void contextLoads()
+   {
+   }
+
+}
